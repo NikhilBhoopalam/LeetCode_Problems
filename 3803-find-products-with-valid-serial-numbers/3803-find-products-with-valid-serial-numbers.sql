2d044595ef7c83ab303c8aa8550f2af417de3168
Nikhil Ravikumar Bhoopalam <66941192+NikhilBhoopalam@users.noreply.github.com>
2025-03-02 13:58:46 -0500

    Time: 545 ms (51.69%), Space: 0B (100%) - LeetHub

---
+++ 3803-find-products-with-valid-serial-numbers/3803-find-products-with-valid-serial-numbers.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+
+# Write your MySQL query statement below
+select * from products where description like '% SN____-____' or description like '% SN____-____ %'
